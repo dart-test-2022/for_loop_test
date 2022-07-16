1041622e5bd626be1f205c7c9fe7b17b4217b34f
sanjarbek <sanjarsaidov173@gamil.com>
2022-07-16 17:12:39 +0500

    sorry

--- for_loop08_test.dart
+++ for_loop08_test.dart
@@ -4,10 +4,10 @@ import '../bin/for_loop08.dart';
 void main() {
   group('for_loop01', (() {
     test('the sum of the numbers', () {
-      expect(func([1, 2, 3, 4, 5], 0, 4), 6);
+      expect(func([1, 2, 3, 4, 5], 0, 4), 9);
     }, timeout: Timeout.factor(2));
     test('the sum of the numbers', () {
-      expect(func([7, 7, 2, 5, 1, 6], 2, 6), 11);
+      expect(func([7, 7, 2, 5, 1, 6], 2, 6), 6);
     }, timeout: Timeout.factor(2));
   }));
 }
